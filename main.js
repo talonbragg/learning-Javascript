97f7700befa9999a004a8c9124f54b70b74ae6de
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-02 11:25:30 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
    for(var x = 25; x < 400; x+=50) {
       for(var y = 25; y < 400; y+=50 ) {
         c = Shape.Circle(x, y, 20);
-        c.fillColor('green');
+        c.fillColor = 'green';
       }
    }
   
