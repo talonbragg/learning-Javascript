4a21dc0e52d58143b2b0c8d02a53b2eede3d5367
Talon Bragg <pvpgamecrash@gmail.com>
2017-02-02 11:15:10 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
   'use strict'
-   console.log('main.js loaded');
+   paper.install(window);
+   paper.setup(document.getElementById('mainCanvas'));
+  
+   //TODO
+  
+   paper.view.draw();
 });
